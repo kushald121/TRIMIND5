43e3f85462c1ffae366f2c9eebf449b2e86e1f64
Kushal Dubey <kushald121@users.noreply.github.com>
2025-10-13 21:31:02 +0530

    Added files

---
+++ Desktop/Chess/my-project/src/app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import ChessGame from "@/components/ChessGame";
+
+export default function HomePage() {
+  return (
+    <main className="min-h-screen bg-gray-50">
+      <ChessGame />
+    </main>
+  );
+}
